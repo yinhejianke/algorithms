23d93efb5b3b66b9ef38bb8e1a412cddaf038034
wushujun <junshuwu@live.cn>
2019-06-04 11:23:13 +0000

    更新快排。。

--- cpp/sorted/sorted.cpp
+++ cpp/sorted/sorted.cpp
@@ -9,6 +9,10 @@
 #include "../sorted.h"
 using namespace std;
 
+//
+//
+//
+
 
 // 冒泡 排序， 顺序与倒序 /
 template <class Tem>
@@ -21,7 +25,6 @@ Tem sorted(Tem array, int PX = 1){
             array[another] = array[index];
             array[index] = Temp;
           }
-
         }
       }
     } else {
@@ -32,17 +35,34 @@ Tem sorted(Tem array, int PX = 1){
             array[another] = array[index];
             array[index] = Temp;
           }
-
         }
       }
     }
   return array;
 }
 
+
 //快速排序
-// template <class Tem>
-// Tem quick_sorted (Tem T, )
 template <class Tem>
-Tem quick_sorte(Tem T){
-  return T;
+void quick_sorte(Tem& arr, int first, int end){
+  int i = first, j = end;
+  auto flag = arr[first];
+  if ( first < end ){
+    while ( i < j ){
+      while ( i < j && arr[j] >= flag ) j--;
+      while ( i < j && arr[i] < flag ) i++;
+      if( i < j ){
+        auto te = arr[i];
+        arr[i] = arr[j];
+        arr[j] = te;
+      }
+    }
+    arr[first] = arr[j];
+    arr[j] = flag;
+    quick_sorte(arr, first, j - 1);
+    quick_sorte(arr, j + 1, end);
+  }
+  else{
+    return ;
+  }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -5,13 +5,18 @@ using namespace std;
 
 int main(){
 
-    string aa = "aaaaaa";
-    cout << aa << endl;
     vector<int> a = {2,2,2, 333,555 ,4,33,53 ,77 ,56};
-    vector<int> cc = sorted(a);
-    for(int i = 0; i < cc.size(); i++){
+    //vector<int> cc = sorted(a);
+    //for(int i = 0; i < cc.size(); i++){
 
-        cout << "cc: " << i << ":  " << cc[i] << endl;
+    //    cout << "cc: " << i << ":  " << cc[i] << endl;
+    //}
+
+    quick_sorte(a, 0, a.size() - 1 );
+    cout << "size: " << a.size() << endl;
+    for(int i = 0; i < a.size(); i++){
+
+        cout << "aa" << i << ": " << a[i] << endl;
     }
     return 0;
 }
